447a2430c98ca4c662e3238ac87cc50cb23d2172
BorhanPetgar <borhanpetgar40@gmail.com>
2023-05-15 11:29:01 +0330

    a typop fixed

--- utils.py
+++ utils.py
@@ -1,18 +1,16 @@
 import cv2
 import numpy as np
 
+
 def get_limits(color):
-    c = np.unit8([[color]]) # here insert the bgr values which you want
+
+    c = np.uint8([[color]]) 
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lower_limit = hsvC[0][0][0] - 10, 100, 100
-    upper_limit = hsvC[0][0][0] + 10, 255, 255
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
 
-    lower_limit = np.array(lower_limit, dtype=np.uint8)
-    upper_limit = np.array(upper_limit, dtype=np.unit8)
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
 
-    return lower_limit, upper_limit
-
-
-a = np.arange(20)
-print(a)
+    return lowerLimit, upperLimit
