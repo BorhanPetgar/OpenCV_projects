a5a449fb49f3b65bbf48c0d927f0b9f65ec537c4
BorhanPetgar <borhanpetgar40@gmail.com>
2023-05-16 11:31:14 +0330

    explanations added

--- utils.py
+++ utils.py
@@ -4,9 +4,13 @@ import numpy as np
 
 def get_limits(color):
 
-    c = np.uint8([[color]]) 
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+    # we will insert the color in BGR format
+    # "c" is a single pixel, its data type is 8 bit unsigned integer
+    c = np.uint8([[color]])  
 
+
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+     
     lowerLimit = hsvC[0][0][0] - 10, 100, 100
     upperLimit = hsvC[0][0][0] + 10, 255, 255
 
